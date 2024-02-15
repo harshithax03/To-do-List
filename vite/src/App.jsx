751c15f95684c4ef37b5377dcb1c15fafc433c38
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-02-15 11:00:19 +0530

    created functionality

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -2,10 +2,33 @@ import { useState } from 'react'
 
 
 function App() {
+  const[todo, setTodo] = useState([])
+ const [task, setTask] = useState('')
+
+ function handleChange(event){
+   setTask(event.target.value)
+
+
+ }
+
+ function addTask(){
+  setTodo([...todo, task])
+   setTask('')
+}
+ function removeTask(index){
+  setTodo(todo.filter((_, i) => i !== index ))
+ 
+}
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+   <>
+   <h1>Your TODO's</h1>
+   <input type="text" onChange={handleChange}/>
+   <button onClick={addTask}>Add</button>
+   <div className='list'>
+     {todo.map((t, index) => <li>{t} <button onClick={()=>removeTask(index)}>X</button></li>)}
+   </div>
+ </>
   )
 }
 
